7bb2bc20be80ff4dbb75a30dc9d0bb3ab6d35fff
Gabriel Caiana <gabrielcaianaguedes@gmail.com>
2023-03-15 04:41:49 +0000

    Pending changes exported from your codespace

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+app.use(express.json());
+const port = 3000;
+
+// books example
+
+const books = [
+  {
+    id: 1,
+    title: 'The Hobbit',
+  },
+  {
+    id: 2,
+    title: 'The Lord of the Rings',
+  },
+];
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.json({ message: 'Hello World!' });
+});
+
+app.get('/books', (req, res) => {
+  res.json(books);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
